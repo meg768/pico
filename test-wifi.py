d9819764b177d0eee5b7cbcfae63fe14525e9b7e
meg768 <meg768@users.noreply.github.com>
2023-03-03 21:41:40 +0100

    -

--- test-wifi.py
+++ test-wifi.py
@@ -2,12 +2,13 @@ from picoled import PicoLED
 from wifi import WiFi
 
 import urequests
+import secrets
 
 led = PicoLED()
 led.blink()
 
 wifi = WiFi()
-wifi.connect(ssid="Julia", password="potatismos")
+wifi.connect(ssid=secrets.SSID, password=secrets.PASSWORD)
 
 print("Fetching astronauts...")
 astronauts = urequests.get("http://api.open-notify.org/astros.json").json()
